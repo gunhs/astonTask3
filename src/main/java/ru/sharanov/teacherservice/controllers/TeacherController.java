b250cb47584def1ef8315944a3caff9e2c844c29
Gunhs <mr-vaska@ya.ru>
2024-03-28 00:42:38 +0500

    ddd

--- src/main/java/ru/sharanov/teacherservice/controllers/TeacherController.java
+++ src/main/java/ru/sharanov/teacherservice/controllers/TeacherController.java
@@ -4,12 +4,17 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import ru.sharanov.teacherservice.dto.StudentResponse;
+import ru.sharanov.teacherservice.dto.TeacherResponse;
+import ru.sharanov.teacherservice.model.School;
 import ru.sharanov.teacherservice.model.Teacher;
+import ru.sharanov.teacherservice.repositories.TeacherRepository;
 import ru.sharanov.teacherservice.services.TeacherService;
 
-import java.util.List;
+import java.util.Optional;
 
 @CrossOrigin("*")
 @RestController
@@ -18,6 +23,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class TeacherController {
     private final TeacherService teacherService;
+    private final TeacherRepository teacherRepository;
 
     @GetMapping("/{id}")
     @Operation(
@@ -25,7 +31,11 @@ public class TeacherController {
             description = "method that returns information about one teacher"
     )
     public ResponseEntity<?> fetchTeacherById(@PathVariable @Parameter(description = "id teacher") Long id) {
-        return teacherService.fetchTeacherById(id);
+        Optional<TeacherResponse> teacherResponse = teacherService.fetchTeacherById(id);
+        if (teacherResponse.isPresent()) {
+            return new ResponseEntity<>(teacherResponse, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
     }
 
     @GetMapping
@@ -44,12 +54,10 @@ public class TeacherController {
     )
     public ResponseEntity<?> createTeacher(
             @RequestBody @Parameter(description = " info about new teacher") Teacher teacher) {
-        return teacherService.createTeacher(teacher);
+        Optional<TeacherResponse> teacherResponse = teacherService.createTeacher(teacher);
+        if (teacherResponse.isPresent()) {
+            return new ResponseEntity<>(teacherResponse, HttpStatus.CREATED);
+        }
+        return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
     }
-    @GetMapping("/students/{studentId}")
-    public List<Teacher> getTeachersByStudentId(@PathVariable Long studentId) {
-        return teacherService.getTeachersByStudentId(studentId);
-    }
-
-
 }
