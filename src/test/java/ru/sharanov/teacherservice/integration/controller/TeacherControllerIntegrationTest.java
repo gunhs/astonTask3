5ec26d071fe8334b0620f19d4bdbf083bb0312ed
Gunhs <mr-vaska@ya.ru>
2024-03-29 20:30:34 +0500

    add test container

--- src/test/java/ru/sharanov/teacherservice/integration/controller/TeacherControllerIntegrationTest.java
+++ src/test/java/ru/sharanov/teacherservice/integration/controller/TeacherControllerIntegrationTest.java
@@ -8,10 +8,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 import ru.sharanov.teacherservice.dto.TeacherResponse;
 import ru.sharanov.teacherservice.dto.TeachersResponse;
 import ru.sharanov.teacherservice.mapper.TeacherMapper;
@@ -27,10 +31,15 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest
+@Testcontainers
 @AutoConfigureMockMvc
 @ActiveProfiles({"test"})
 public class TeacherControllerIntegrationTest {
+
+    @Container
+    @ServiceConnection
+    static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:12.0");
 
     @Autowired
     private MockMvc mockMvc;
